98dabb295d3fcede015808b180dbf8a5cbe99f02
Anoop Vasudevan <postme.av@gmail.com>
2020-09-20 16:58:08 -0400

    Updates

--- precache-manifest.6441cbf25431bbf449aceb3a0289dafb.js
+++ precache-manifest.6441cbf25431bbf449aceb3a0289dafb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a0120497ffad6af43f3904c4e30d9233",
+    "revision": "8b54761f9a4594dff8c4cdf637fe2070",
     "url": "/teams-api/index.html"
   },
   {
-    "revision": "10ff074ca1c70cdc7086",
+    "revision": "651c17011fe414f31a2b",
     "url": "/teams-api/static/css/main.03441e0d.chunk.css"
   },
   {
-    "revision": "f841f8503f79f9efcea8",
-    "url": "/teams-api/static/js/2.a88b7dbe.chunk.js"
+    "revision": "77576ba0ba1682322495",
+    "url": "/teams-api/static/js/2.7a5c4745.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/teams-api/static/js/2.a88b7dbe.chunk.js.LICENSE.txt"
+    "url": "/teams-api/static/js/2.7a5c4745.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "10ff074ca1c70cdc7086",
-    "url": "/teams-api/static/js/main.cea2e4cf.chunk.js"
+    "revision": "651c17011fe414f31a2b",
+    "url": "/teams-api/static/js/main.cd41a167.chunk.js"
   },
   {
     "revision": "88614d34e399610e66a0",
